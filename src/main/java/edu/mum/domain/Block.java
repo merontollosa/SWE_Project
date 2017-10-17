f8ac6aacdc085b27feb3dfe777a09e8ef4638dea
woldt <woldtensae.aman@gmail.com>
2017-10-16 21:33:37 -0500

    adding block and entry entities to the domain package

---
+++ src/main/java/edu/mum/domain/Block.java
@@ -0,0 +1,76 @@
+package edu.mum.domain;
+
+import java.sql.Date;
+
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Block {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String blockMonth;
+	private Date blockStartDate;
+	private Date blockEndDate;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "entry_id")
+	private Entry entry;
+	
+	//private List<Section> sections;
+
+	public long getId() {
+		return id;
+	}
+
+	public Entry getEntry() {
+		return entry;
+	}
+
+	public void setEntry(Entry entry) {
+		this.entry = entry;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBlockMonth() {
+		return blockMonth;
+	}
+
+	public void setBlockMonth(String blockMonth) {
+		this.blockMonth = blockMonth;
+	}
+
+	public Date getBlockStartDate() {
+		return blockStartDate;
+	}
+
+	public void setBlockStartDate(Date blockStartDate) {
+		this.blockStartDate = blockStartDate;
+	}
+
+	public Date getBlockEndDate() {
+		return blockEndDate;
+	}
+
+	public void setBlockEndDate(Date blockEndDate) {
+		this.blockEndDate = blockEndDate;
+	}
+
+	/*public List<Section> getSections() {
+		return sections;
+	}
+
+	public void setSections(List<Section> sections) {
+		this.sections = sections;
+	}*/
+}
